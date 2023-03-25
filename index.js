ae19f114da8d202226522f1e2997bc141c3c7b73
gameterror6214 <128888173+gameterror6214@users.noreply.github.com>
2023-03-25 10:10:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function goToTELEG() {
+    window.location.href = 'https://web.telegram.org/k/#-1884551300';
+  }
+ 
+  function Support() {
+    alert('Для быстрого ответа напишите последним словом "HMH"');
+    window.location.href = 'https://web.telegram.org/k/#@Denchick8';
+  }
